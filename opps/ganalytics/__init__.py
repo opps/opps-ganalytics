73243623207bed683b712617fe53ba0adf09c05f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-26 16:53:21 -0300

    Set global settings var ganalytics

--- opps/ganalytics/__init__.py
+++ opps/ganalytics/__init__.py
@@ -1,5 +1,7 @@
 import pkg_resources
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
 
 pkg_resources.declare_namespace(__name__)
 
@@ -14,3 +16,11 @@ __author__ = u"Thiago Avelino"
 __credits__ = []
 __email__ = u"thiagoavelinoster@gmail.com"
 __copyright__ = u"Copyright 2013, YACOWS"
+
+
+settings.OPPS_GANALYTICS_ACCOUNT = getattr(settings,
+                                           'OPPS_GANALYTICS_ACCOUNT', '')
+settings.OPPS_GANALYTICS_PASSWORD = getattr(settings,
+                                            'OPPS_GANALYTICS_PASSWORD', '')
+settings.OPPS_GANALYTICS_APIKEY = getattr(settings,
+                                          'OPPS_GANALYTICS_APIKEY', '')
