b06367f0a07949338691ead164e744ccfeb29540
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-27 17:59:29 -0300

    Remove django import on init ganalytics

--- opps/ganalytics/__init__.py
+++ opps/ganalytics/__init__.py
@@ -3,7 +3,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
@@ -12,18 +12,3 @@ __author__ = u"Thiago Avelino"
 __credits__ = []
 __email__ = u"thiagoavelinoster@gmail.com"
 __copyright__ = u"Copyright 2013, YACOWS"
-
-
-try:
-    from django.utils.translation import ugettext_lazy as _
-    from django.conf import settings
-    trans_app_label = _(u'Google Analytics')
-
-    settings.OPPS_GANALYTICS_ACCOUNT = getattr(settings,
-                                               'OPPS_GANALYTICS_ACCOUNT', '')
-    settings.OPPS_GANALYTICS_PASSWORD = getattr(settings,
-                                                'OPPS_GANALYTICS_PASSWORD', '')
-    settings.OPPS_GANALYTICS_APIKEY = getattr(settings,
-                                              'OPPS_GANALYTICS_APIKEY', '')
-except:
-    pass
