ba7df10cfa694037681aa6c138be35d1f48eef76
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-27 17:43:05 -0300

    fix admin ganalytics (basic)

---
+++ opps/ganalytics/admin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Filter, Query, QueuryFilter
+
+
+class QueuryFilterInline(admin.TabularInline):
+    model = QueuryFilter
+    fk_name = 'query'
+    raw_id_fields = ['filter']
+    actions = None
+    extra = 1
+    fieldsets = [(None, {
+        'classes': ('collapse',),
+        'fields': ('filter', 'order')})]
+
+
+class QueryAdmin(admin.ModelAdmin):
+    inlines = [QueuryFilterInline]
+
+admin.site.register(Filter)
+admin.site.register(Query, QueryAdmin)
