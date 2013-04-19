6b0662fcd5a97f1bec029568171c32f160ba34f2
Rael Max <contato@raelmax.com>
2013-04-19 18:57:21 -0300

    get_top_read templatetag filtering by child_class

--- opps/ganalytics/templatetags/ganalytics_tags.py
+++ opps/ganalytics/templatetags/ganalytics_tags.py
@@ -7,12 +7,15 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_top_read(number=10, channel_slug=None,
+def get_top_read(number=10, channel_slug=None, child_class=None,
                  template_name='ganalytics/top_read.html'):
 
     top_read = Report.objects.filter(article__isnull=False).order_by('-pageview')
     if channel_slug:
         top_read = top_read.filter(article__channel_long_slug=channel_slug)
+
+    if child_class:
+        top_read = top_read.filter(article__child_class=child_class)
 
     top_read = top_read[:number]
 
