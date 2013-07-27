a4065f0fb2fc7b2c7442e0e1d338d900ac90a2d9
Ellison Leao <ellisonleao@gmail.com>
2013-07-27 01:54:56 -0300

    Fix ganalytics tags on branch

--- opps/ganalytics/templatetags/ganalytics_tags.py
+++ opps/ganalytics/templatetags/ganalytics_tags.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
+
 from django import template
+from django.conf import settings
+from django.db.models import Sum
+from django.utils import timezone
 from opps.ganalytics.models import Report
+from opps.articles.models import Article
 
 
 register = template.Library()
 
 
-@register.simple_tag
-def get_top_read(number=10, channel_slug=None, child_class=None,
+@register.simple_tag(takes_context=True)
+def get_top_read(context, number=10, channel_slug=None, child_class=None,
                  template_name='ganalytics/top_read.html'):
 
+    now = timezone.now()
+    start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
+
     top_read = Report.objects.filter(
-        article__isnull=False
+        article__isnull=False,
+        article__date_available__range=(start, now),
+        article__published=True,
     ).order_by('-pageview')
 
     if channel_slug:
@@ -22,10 +33,56 @@ def get_top_read(number=10, channel_slug=None, child_class=None,
     if child_class:
         top_read = top_read.filter(article__child_class=child_class)
 
-    top_read = top_read.distinct()[:number]
+    # to avoid repetitions annotate acts like group_by
+    top_read = top_read.distinct().values(
+        'article'
+    ).annotate(Sum('pageview'))[:number]
+
+    # as values returns a dict with pk only, needs to get the instance
+    for top in top_read:
+        top['article'] = Article.objects.get(pk=top['article'])
 
     t = template.loader.get_template(template_name)
 
     return t.render(template.Context({'top_read': top_read,
                                       'channel_slug': channel_slug,
-                                      'number': number}))
+                                      'number': number,
+                                      'context': context}))
+
+
+@register.simple_tag(takes_context=True)
+def get_channels_top_read(context, *channels, **kwargs):
+    now = timezone.now()
+    start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
+
+    top_read = Report.objects.filter(
+        article__isnull=False,
+        article__published=True,
+        article__date_available__range=(start, now),
+        article__channel_long_slug__in=channels,
+    ).order_by('-pageview')
+
+    top_read = top_read.distinct().values(
+        'article'
+    ).annotate(Sum('pageview'))
+    tops = {}
+
+    #get one from each channel
+    for top in top_read:
+        article = Article.objects.get(pk=top['article'])
+        top['article'] = article
+        if not article.channel_long_slug in tops:
+            tops[article.channel_long_slug] = top
+
+    ordered = OrderedDict(
+        sorted(tops.items(),
+               key=lambda item: item[1]['pageview__sum'],
+               reverse=True)
+    )
+    template_name = kwargs.get('template_name',
+                               'ganalytics/channel_top_read.html')
+
+    t = template.loader.get_template(template_name)
+
+    return t.render(template.Context({'top_read': ordered,
+                                      'context': context}))
