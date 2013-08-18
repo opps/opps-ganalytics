cd6a7d7d6701b2bc7558d3a3dca624ef53a3ea3d
Davi Campos <arlocm@gmail.com>
2013-08-18 00:12:37 -0300

    fixing migrations dependencies to use postgresql

--- opps/ganalytics/migrations/0001_initial.py
+++ opps/ganalytics/migrations/0001_initial.py
@@ -10,6 +10,10 @@ User = get_user_model()
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("containers", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'Filter'
         db.create_table(u'ganalytics_filter', (
