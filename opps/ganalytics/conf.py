ced7e182a2e189325c65e7d25507e43cf082fbd3
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-27 17:09:54 -0300

    Create conf app

---
+++ opps/ganalytics/conf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+from appconf import AppConf
+
+
+class OppsGAnalyticsConf(AppConf):
+
+    RUN_EVERY_HOUR = getattr(
+        settings, 'OPPS_GANALYTICS_RUN_EVERY_HOUR', 4)
+    RUN_EVERY_MINUTE = getattr(
+        settings, 'OPPS_GANALYTICS_RUN_EVERY_MINUTE', 30)
+    RUN_EVERY_DAY_OF_WEEK = getattr(
+        settings, 'OPPS_GANALYTICS_RUN_EVERY_DAY_OF_WEEK', "*")
+
+    ACCOUNT = getattr(
+        settings, "OPPS_GANALYTICS_ACCOUNT", "")
+    PASSWORD = getattr(
+        settings, "OPPS_GANALYTICS_PASSWORD", "")
+    APIKEY = getattr(
+        settings, "OPPS_GANALYTICS_APIKEY", "")
+
+    class Meta:
+        prefix = 'opps_ganalytics'
