6dc51cfe2fa94b002e1fedc73c0e5e26194bc7c8
Thiago Avelino <thiago@avelino.xxx>
2013-11-18 13:41:12 -0200

    Add cache on templatetag render

--- opps/ganalytics/templatetags/ganalytics_tags.py
+++ opps/ganalytics/templatetags/ganalytics_tags.py
@@ -5,6 +5,8 @@ from django import template
 from django.conf import settings
 from django.db.models import Sum
 from django.utils import timezone
+from django.core.cache import cache
+
 from opps.ganalytics.models import Report
 from opps.containers.models import Container
 
@@ -15,6 +17,21 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_top_read(context, number=10, channel_slug=None, child_class=None,
                  template_name='ganalytics/top_read.html'):
+
+    request = context['request']
+    is_mobile = getattr(request, 'is_mobile', False)
+
+    cachekey = "ga-gettopread-{}{}{}{}{}".format(
+        number,
+        channel_slug,
+        child_class,
+        template_name,
+        is_mobile
+    )
+
+    render = cache.get(cachekey)
+    if render:
+        return render
 
     now = timezone.now()
     start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
@@ -44,14 +61,35 @@ def get_top_read(context, number=10, channel_slug=None, child_class=None,
 
     t = template.loader.get_template(template_name)
 
-    return t.render(template.Context({'top_read': top_read,
-                                      'channel_slug': channel_slug,
-                                      'number': number,
-                                      'context': context}))
+    render = t.render(template.Context({
+        'top_read': top_read,
+        'channel_slug': channel_slug,
+        'number': number,
+        'context': context}))
+
+    cache.set(cachekey, render, settings.OPPS_CACHE_EXPIRE*60)
+
+    return render
 
 
 @register.simple_tag(takes_context=True)
 def get_channels_top_read(context, *channels, **kwargs):
+    template_name = kwargs.get('template_name',
+                               'ganalytics/channel_top_read.html')
+
+    request = context['request']
+    is_mobile = getattr(request, 'is_mobile', False)
+
+    cachekey = "ga-gettopread-{}{}{}".format(
+        "-".join(channels),
+        template_name,
+        is_mobile
+    )
+
+    render = cache.get(cachekey)
+    if render:
+        return render
+
     now = timezone.now()
     start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
 
@@ -79,10 +117,12 @@ def get_channels_top_read(context, *channels, **kwargs):
                key=lambda item: item[1]['pageview__sum'],
                reverse=True)
     )
-    template_name = kwargs.get('template_name',
-                               'ganalytics/channel_top_read.html')
-
     t = template.loader.get_template(template_name)
 
-    return t.render(template.Context({'top_read': ordered,
-                                      'context': context}))
+    render = t.render(template.Context(
+        {'top_read': ordered,
+         'context': context}))
+
+    cache.set(cachekey, render, settings.OPPS_CACHE_EXPIRE*60)
+
+    return render
