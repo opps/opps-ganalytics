9d9d82e802091723110cfb6b7e3506fe9eb4c36c
Ellison Leao <ellisonleao@gmail.com>
2014-01-09 18:42:58 -0200

    Add cache on templatetags / change get_channel_top_read approach

--- opps/ganalytics/templatetags/ganalytics_tags.py
+++ opps/ganalytics/templatetags/ganalytics_tags.py
@@ -5,6 +5,7 @@ from django import template
 from django.conf import settings
 from django.db.models import Sum
 from django.utils import timezone
+from django.core.cache import cache
 
 from opps.ganalytics.models import Report
 from opps.articles.models import Article
@@ -17,6 +18,21 @@ SMART_SEARCH = getattr(settings, 'OPPS_GANALYTICS_SMART_SEARCH', True)
 @register.simple_tag(takes_context=True)
 def get_top_read(context, number=10, channel_slug=None, child_class=None,
                  template_name='ganalytics/top_read.html'):
+
+    request = context['request']
+    is_mobile = getattr(request, 'is_mobile', False)
+
+    cachekey = "ga-gettopread-{}{}{}{}{}".format(
+        number,
+        channel_slug,
+        child_class,
+        template_name,
+        is_mobile
+    )
+
+    render = cache.get(cachekey)
+    if render:
+        return render
 
     now = timezone.now()
     start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
@@ -55,45 +71,56 @@ def get_top_read(context, number=10, channel_slug=None, child_class=None,
 
     t = template.loader.get_template(template_name)
 
-    return t.render(template.Context({'top_read': top_read,
-                                      'channel_slug': channel_slug,
-                                      'number': number,
-                                      'context': context}))
-
+    render = t.render(template.Context({'top_read': top_read,
+                                        'channel_slug': channel_slug,
+                                        'number': number,
+                                        'context': context}))
+    cache.set(cachekey, render, 60 * 60)
+    return render
 
 @register.simple_tag(takes_context=True)
 def get_channels_top_read(context, *channels, **kwargs):
+    template_name = kwargs.get('template_name',
+                               'ganalytics/channel_top_read.html')
+    request = context['request']
+    is_mobile = getattr(request, 'is_mobile', False)
+
+    cachekey = "ga-gettopread-{}{}{}".format(
+        "-".join(channels),
+        template_name,
+        is_mobile
+    )
+
+    render = cache.get(cachekey)
+    if render:
+        return render
+
     now = timezone.now()
     start = now - timezone.timedelta(days=settings.OPPS_GANALYTICS_RANGE_DAYS)
 
-    top_read = Report.objects.filter(
-        article__isnull=False,
-        article__published=True,
-        article__date_available__range=(start, now),
-        article__channel_long_slug__in=channels,
-    ).order_by('-pageview')
-
-    top_read = top_read.distinct().values(
-        'article'
-    ).annotate(Sum('pageview'))
     tops = {}
-
-    #get one from each channel
-    for top in top_read:
-        article = Article.objects.get(pk=top['article'])
-        top['article'] = article
-        if not article.channel_long_slug in tops:
-            tops[article.channel_long_slug] = top
+    for channel in channels:
+        try:
+            top_read = Report.objects.filter(
+                article__isnull=False,
+                article__published=True,
+                article__date_available__range=(start, now),
+                article__channel_long_slug__contains=channel,
+            ).order_by('-pageview', '-article__date_available').latest('id')
+        except Report.DoesNotExist:
+            tops[channel] = None
+            continue
+        tops[channel] = top_read
 
     ordered = OrderedDict(
-        sorted(tops.items(),
-               key=lambda item: item[1]['pageview__sum'],
-               reverse=True)
+        sorted(tops.items(), key=lambda item: item[1].pageview, reverse=True)
     )
     template_name = kwargs.get('template_name',
                                'ganalytics/channel_top_read.html')
 
     t = template.loader.get_template(template_name)
 
-    return t.render(template.Context({'top_read': ordered,
-                                      'context': context}))
+    render = t.render(template.Context({'top_read': ordered,
+                                        'context': context}))
+    cache.set(cachekey, render, 60 * 60)
+    return render
