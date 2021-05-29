0141f9b3b5d913a09aca99365cdcc53167865a7d
Avelino <avelinorun@gmail.com>
2021-05-29 09:55:35 -0300

    generate new version 0.2.6
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- opps/ganalytics/__init__.py
+++ opps/ganalytics/__init__.py
@@ -1,14 +1,13 @@
 import pkg_resources
 
-
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 2, 5)
+VERSION = (0, 2, 6)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
 __description__ = u"Google Analytics for Opps CMS"
 __author__ = u"Thiago Avelino"
 __credits__ = ['Jean Rodrigues']
-__email__ = u"thiagoavelinoster@gmail.com"
-__copyright__ = u"Copyright 2015, YACOWS"
+__email__ = u"avelinorun@gmail.com"
+__copyright__ = u"Copyright 2015, Opps CMS"
